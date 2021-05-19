142e48102e29fcb5da7b1ddb850778aa98e338b4
Kiru <mail@kiru.gay>
2021-05-19 16:49:13 +0000

    new: Error handling and directory listings
    All error, pages and alike will use the `main.css` however some may get
    their own in the future.

--- src/main.js
+++ src/main.js
@@ -1,20 +1,27 @@
 const fs = require("fs");
 const path = require("path");
 const http = require("http");
+const css = {
+	normal: fs.readFileSync(path.join(__dirname + "/main.css"), "utf8")
+}
 
 http.createServer((req, res) => {
 	reqPath = path.join("site" + req.url)
 	fs.readFile(reqPath, (err,data) => {
 		if (err) {
+			res.write(`<style>${css.normal}</style>`);
 			switch(err.code) {
-				case "ENOENT":
-					res.writeHead(404);
-					res.end(JSON.stringify(err));
-					break;
 				case "EISDIR":
 					res.writeHead(200)
-					res.end("Directory listing:")
+					res.write(`<b>Directory listing for ${reqPath}</b><br>`)
+					fs.readdirSync(reqPath, (data)).forEach(i => {
+						res.write(`<br><a href="${i}">${i}</a>`)
+					})
+					res.end("")
 					break;
+				default:
+					res.writeHead(404);
+					res.end(JSON.stringify(err));
 			}
 			return;
 		}
