2fd4d8af9fc78c15d5da29aaf9aeb5f4f25caeaa
Kiru <mail@kiru.gay>
2021-05-19 16:56:33 +0000

    fix: Missing semiclons
    I sometimes forget to put them as they're optional but imo they look
    better but again I'm very bad at remembering.

--- src/main.js
+++ src/main.js
@@ -1,23 +1,24 @@
 const fs = require("fs");
 const path = require("path");
 const http = require("http");
+
 const css = {
 	normal: fs.readFileSync(path.join(__dirname + "/main.css"), "utf8")
 }
 
 http.createServer((req, res) => {
-	reqPath = path.join("site" + req.url)
+	reqPath = path.join("site" + req.url);
 	fs.readFile(reqPath, (err,data) => {
 		if (err) {
 			res.write(`<style>${css.normal}</style>`);
 			switch(err.code) {
 				case "EISDIR":
-					res.writeHead(200)
-					res.write(`<b>Directory listing for ${reqPath}</b><br>`)
+					res.writeHead(200);
+					res.write(`<b>Directory listing for ${reqPath}</b><br>`);
 					fs.readdirSync(reqPath, (data)).forEach(i => {
-						res.write(`<br><a href="${i}">${i}</a>`)
+						res.write(`<br><a href="${i}">${i}</a>`);
 					})
-					res.end("")
+					res.end("");
 					break;
 				default:
 					res.writeHead(404);
