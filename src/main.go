08ce0f748d25c5095cd0dbde2d4e26117f89871f
Kiru <mail@kiru.gay>
2022-04-08 21:37:43 +0200

    implemented nearly all settings
    The only settings missing are authentication and htpasswd, for the same
    reasons, I also took the liberty to rename noerrorpage to no_errorpage
    to make it more readable and for it to look like all the other settings
    as well, as they all were using snake case as well.
    
    I also disabled the ability to use booleans, and now only arrays are
    acceptable, however not providing a value is the same as false, and
    putting something like '[".*"]' is the same as true. It's just for more
    flexibility and better codebase.
    
    authentication will also be turning into an array compared to it's only
    boolean value before, meaning you can require authentication on some
    requests but not all.

--- src/main.go
+++ src/main.go
@@ -29,7 +29,23 @@ error {
 }
 </style>`
 
-func handler(port string, path string, site string) http.HandlerFunc {
+type Site struct {
+	Port int
+	Path string
+	Site string
+
+	Htpasswd string
+	Authentication []string
+
+	HideFiles []string `json:"hide_files"`
+	ShowFiles []string `json:"show_files"`
+	AllowFiles []string `json:"allow_files"`
+	BlockFiles []string `json:"block_files"`
+	NoErrorPage []string `json:"no_errorpage`
+	NoFilelistings []string `json:"no_filelistings"`
+}
+
+func handler(port string, path string, site string, config Site) http.HandlerFunc {
 	fmt.Println(":: serving \"" + site + "\" on port " + port)
 	return func(w http.ResponseWriter, r *http.Request) {
 		var url = r.URL.String();
@@ -43,6 +59,11 @@ func handler(port string, path string, site string) http.HandlerFunc {
 
 		var error = func(code uint, error string, msg string) {
 			w.WriteHeader(404);
+			for _, i := range config.NoErrorPage {
+				if (regexp.MustCompile(i).MatchString(url)) {
+					return;
+				}
+			}
 
 			addcss();
 
@@ -50,15 +71,56 @@ func handler(port string, path string, site string) http.HandlerFunc {
 			fmt.Fprint(w, "<error>" + msg + "</error>")
 		}
 
+		var notfound = func() {
+			error(404, "An error occurred", "File not found!");
+		}
+
+		var serve = func() {
+			if (len(config.BlockFiles) == 0 &&
+				len(config.AllowFiles) == 0) {
+
+				http.ServeFile(w, r, req);
+				return;
+			}
+
+			for _, i := range config.BlockFiles {
+				if (regexp.MustCompile(i).MatchString(url)) {
+					notfound();
+					return;
+				}
+			}
+
+			if (len(config.AllowFiles) == 0) {
+				http.ServeFile(w, r, req)
+			} else {
+				for _, i := range config.AllowFiles {
+					if (regexp.MustCompile(i).MatchString(url)) {
+						http.ServeFile(w, r, req);
+						return;
+					}
+				}
+
+				notfound();
+			}
+		}
+
 		w.Header().Set("Content-Type", "text/html; charset=utf-8")
 		if (! os.IsNotExist(err)) {
 			if (! file.IsDir()) {
-				http.ServeFile(w, r, req)
+				serve();
 			} else {
 				file, err = os.Stat(req + "/index.html");
 				if (! os.IsNotExist(err)) {
-					http.ServeFile(w, r, req + "/index.html")
-					return
+					url = url + "/index.html"
+					serve();
+					return;
+				}
+
+				for _, i := range config.NoFilelistings {
+					if (regexp.MustCompile(i).MatchString(url)) {
+						notfound();
+						return;
+					}
 				}
 
 				var files, err = ioutil.ReadDir(req);
@@ -69,7 +131,23 @@ func handler(port string, path string, site string) http.HandlerFunc {
 
 				addcss();
 				fmt.Fprint(w, "<b>Directory listing for " + r.URL.String() + "</b><br><br>")
+
+				files:
 				for _, i := range files {
+					for _, ii := range config.HideFiles {
+						if (regexp.MustCompile(ii).MatchString(i.Name())) {
+							continue files;
+						}
+					}
+					
+					if (len(config.ShowFiles) > 0) {
+						for _, ii := range config.ShowFiles {
+							if (! regexp.MustCompile(ii).MatchString(i.Name())) {
+								continue files;
+							}
+						}
+					}
+
 					if (i.IsDir()) {
 						dirsArr = append(dirsArr, i.Name());
 					} else {
@@ -87,15 +165,15 @@ func handler(port string, path string, site string) http.HandlerFunc {
 				}
 			}
 		} else {
-			error(404, "An error occurred", "File not found!")
+			notfound();
 		}
 	}
 }
 
-func server(port string, path string, site string) {
+func server(port string, path string, site string, config Site) {
 	s := &http.Server{
 		Addr: ":" + port,
-		Handler: http.HandlerFunc(handler(port, path, site)),
+		Handler: http.HandlerFunc(handler(port, path, site, config)),
 
 		MaxHeaderBytes: 1 << 20,
 		ReadTimeout:    10 * time.Second,
@@ -114,29 +192,13 @@ func main() {
 			var port = regexp.MustCompile(":.*$").FindString(i);
 			if (port == "") {port = ":8080";path = i + ":"}
 
+			var empty Site;
 			path = path[:len(path)-1];
-			server(port[1:], path, path)
+			server(port[1:], path, path, empty)
 		}
 	}
 
 	if (len(os.Args[1:]) == 0) {
-		type Site struct {
-			Port int
-			Path string
-			Site string
-
-			Htpasswd string
-			Authentication bool
-
-			NoErrorPage bool
-			NoFilelistings bool
-
-			HideFiles []string
-			ShowFiles []string
-			AllowFiles []string
-			BlockFiles []string
-		}
-
 		file, err := ioutil.ReadFile("/etc/kitty/sites.json");
 		if (err != nil) {panic(err)}
 
@@ -144,7 +206,7 @@ func main() {
 		json.Unmarshal(file, &sites)
 
 		for _, i := range sites {
-			server(fmt.Sprintf("%d", i.Port), i.Path, i.Site)
+			server(fmt.Sprintf("%d", i.Port), i.Path, i.Site, i)
 		}
 	}
 
